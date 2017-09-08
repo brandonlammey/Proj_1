ede0875a0ceed1f15663fcf732200660c59afd86
Kaiser Mittenburg <kaisermittenburg@gmail.com>
2017-09-08 14:53:36 -0500

    Added php code. Renamed files with new php extension

--- user.php
+++ user.php
@@ -1,3 +1,18 @@
+<?php
+session_start();
+ob_start();
+
+$host="sql9.freemysqlhosting.net"; // Host name
+$username="sql9193383"; // Mysql username
+$password="5Die3BXFPm"; // Mysql password
+$db_name="sql9193383"; // Database name
+$tbl_name="Project_1_Table_1"; // Table name
+
+// Connect to server and select databse.
+mysql_connect("$host", "$username", "$password")or die("cannot connect");
+mysql_select_db("$db_name")or die("cannot select DB");
+?>
+
 <html>
   <head>
 
