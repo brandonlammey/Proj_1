35968e72315dfec6b8a4599a51a250505aefb37c
Kaiser Mittenburg <kaisermittenburg@gmail.com>
2017-09-13 11:19:49 -0500

    SAVING WORKS WITH NEW FORMATgit add js/admin.js php/getEventsFromDB.php

---
+++ php/getEventsFromDB.php
@@ -0,0 +1,17 @@
+<?php
+include 'header.php';
+
+$result = mysql_query( "SELECT * FROM $tbl_name");
+$arr = [];
+while ($row = mysql_fetch_assoc($result))
+{
+   $arr2 = array();
+   foreach ($row as $val) $arr2[] = $val;
+   $arr[] = $arr2;
+};
+echo $arr[0][2];
+
+ ?>
+
+var jArray= <?php echo json_encode($phpArray ); ?>;
+javascript code to get php array
