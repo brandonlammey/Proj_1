5706b526ad9d14cfc72325fd4e6fb5543aed2427
James Glass <jamesglass@Jamess-MacBook-Pro.local>
2017-09-29 21:56:02 -0500

    Added "Add Another Day" button with functionality

--- js/admin.js
+++ js/admin.js
@@ -8,11 +8,11 @@
 
 {
   /**
-   * Array to store information for all events; populated whenever page loads 
+   * Array to store information for all events; populated whenever page loads
    */
   var events_arr;
 
-  /** 
+  /**
  * Fetches events info from database and populates events_arr
  * @param {string} path - file name that event info is obtained from
  * @param {function} callback - function that can be defined while calling getEventsFromDB to perform required task
@@ -40,8 +40,8 @@
       request.send();
       return request.onreadystatechange();
   }
-  
-  
+
+
   //Call script
   getEventsFromDB('php/getEventsFromDB.php', /**
    * Debugging function that prints event detais to the console
@@ -53,9 +53,9 @@
       });
 }
 
-/** 
- * Array that stores information about the status of every 30-minute time interval. 
- * At every array index: object{active : boolean bit to denote whether that time interval is going to have an event during it or not  
+/**
+ * Array that stores information about the status of every 30-minute time interval.
+ * At every array index: object{active : boolean bit to denote whether that time interval is going to have an event during it or not
  *                              attendees: Set to organizer's name if the time interval is active, aet to "0" otherwise}
  */
 var time_arr = [];
@@ -67,7 +67,7 @@ for(let i=0; i<48; i++)
 
 /**
  * Resets the color of all the buttons for every time interval to its default setting.
- * @param {number} col_num - Index corresponding to a time interval in "time_arr"  
+ * @param {number} col_num - Index corresponding to a time interval in "time_arr"
  */
 function colorReset(col_num){
   if(col_num==0){$("#am1200").css("background-color","#303030"); $("#t0000").css("background-color", "#303030")
@@ -208,7 +208,7 @@ function timeReset()
 
 /**
  * Collects input from form elements and the time table buttons and sends it to the database.
- * @param {object} form - HTML form element that takes user input for Organizer Name, Event Name, Event Date, and Event Times 
+ * @param {object} form - HTML form element that takes user input for Organizer Name, Event Name, Event Date, and Event Times
  */
 function formData(form)
 {
@@ -417,9 +417,12 @@ function formData(form)
             time = "0";
             break;
         }
-        
+
         addOns += time + "=" + JSON.stringify(attendees) + "&";
 
+        //make admin able to change event_name and admin_name again
+        document.getElementById('admin_name').readOnly = false;
+        document.getElementById('event_name').readOnly = false;
     }
     //alert(addOns); //debugging
 
@@ -440,6 +443,242 @@ function formData(form)
     }
     //Reload page. FORCE RELOAD (do not reload from cache) is true,
     location.reload(true);
+}
+
+function formDataAndNext(form)
+{
+    //make admin unable to change event_name and admin_name
+    document.getElementById('admin_name').readOnly = true;
+    document.getElementById('event_name').readOnly = true;
+
+    let name = form.admin_name.value;
+    let ev_name = form.event_name.value;
+    let date = form.event_date.value;
+    let timecheck = false;
+    for(let i=0; i<48; i++)
+    {
+        console.log(time_arr[i].active);
+        if(time_arr[i].active)
+        {
+            timecheck = true;
+            time_arr[i].attendees = name + ", ";
+        }
+    }
+    if(name=="" || ev_name=="" || date=="" || (timecheck==false))
+    {
+        alert("All fields are mandatory!");
+    }
+    else
+    {
+    /////////////////////////////////////////////////////////////////////
+    //Adapted from code found at https://www.w3schools.com/php/php_ajax_database.asp
+    // 9/10/2017 15:25
+
+    if (window.XMLHttpRequest)
+    {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    { // code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (this.readyState==4 && this.status==200)
+        {
+        document.getElementById("txtHint").innerHTML=this.responseText;
+        }
+    }
+    var PageToSendTo = "php/saveEventToDB.php?";
+
+    var addOns = "";
+    for(var i = 0; i < 48; i++)
+    {
+        let attendees;
+        if(time_arr[i].active)
+        {
+          attendees = "" + time_arr[i].attendees;
+        }
+        else
+        {
+            attendees = "0";
+        }
+
+        let time = " ";
+        switch(i)
+        {
+          case 0:
+            time = "0000";
+            break;
+          case 1:
+            time = "0030";
+            break;
+          case 2:
+            time = "0100";
+            break;
+          case 3:
+            time = "0130";
+            break;
+          case 4:
+            time = "0200";
+            break;
+          case 5:
+            time = "0230";
+            break;
+          case 6:
+            time = "0300";
+            break;
+          case 7:
+            time = "0330";
+            break;
+          case 8:
+            time = "0400";
+            break;
+          case 9:
+            time = "0430";
+            break;
+          case 10:
+            time = "0500";
+            break;
+          case 11:
+            time = "0530";
+            break;
+          case 12:
+            time = "0600";
+            break;
+          case 13:
+            time = "0630";
+            break;
+          case 14:
+            time = "0700";
+            break;
+          case 15:
+            time = "0730";
+            break;
+          case 16:
+            time = "0800";
+            break;
+          case 17:
+            time = "0830";
+            break;
+          case 18:
+            time = "0900";
+            break;
+          case 19:
+            time = "0930";
+            break;
+          case 20:
+            time = "1000";
+            break;
+          case 21:
+            time = "1030";
+            break;
+          case 22:
+            time = "1100";
+            break;
+          case 23:
+            time = "1130";
+            break;
+          case 24:
+            time = "1200";
+            break;
+          case 25:
+            time = "1230";
+            break;
+          case 26:
+            time = "1300";
+            break;
+          case 27:
+            time = "1330";
+            break;
+          case 28:
+            time = "1400";
+            break;
+          case 29:
+            time = "1430";
+            break;
+          case 30:
+            time = "1500";
+            break;
+          case 31:
+            time = "1530";
+            break;
+          case 32:
+            time = "1600";
+            break;
+          case 33:
+            time = "1630";
+            break;
+          case 34:
+            time = "1700";
+            break;
+          case 35:
+            time = "1730";
+            break;
+          case 36:
+            time = "1800";
+            break;
+          case 37:
+            time = "1830";
+            break;
+          case 38:
+            time = "1900";
+            break;
+          case 39:
+            time = "1930";
+            break;
+          case 40:
+            time = "2000";
+            break;
+          case 41:
+            time = "2030";
+            break;
+          case 42:
+            time = "2100";
+            break;
+          case 43:
+            time = "2130";
+            break;
+          case 44:
+            time = "2200";
+            break;
+          case 45:
+            time = "2230";
+            break;
+          case 46:
+            time = "2300";
+            break;
+          case 47:
+            time = "2330";
+            break;
+          default:
+            time = "0";
+            break;
+        }
+
+        addOns += time + "=" + JSON.stringify(attendees) + "&";
+
+    }
+    //alert(addOns); //debugging
+
+    var UrlToSend = PageToSendTo + "name=" + JSON.stringify(name) + "&event_name=" + JSON.stringify(ev_name) + "&date=" + JSON.stringify(date) + "&" + addOns;
+
+    //console.log(JSON.stringify(name));
+    xmlhttp.open("GET", UrlToSend, false);
+    xmlhttp.send();
+    //END ADAPTATION
+    //////////////////////////////////////////////////////////////////////
+    //form.reset();
+    timeReset();
+
+    //Reset all time colors
+    for(var i=0;i<48;i++){
+      colorReset(i);
+    }
+    }
+    //Reload page. FORCE RELOAD (do not reload from cache) is true,
+    //location.reload(true);
 }
 
 /**
@@ -636,7 +875,7 @@ function makeTable12(n)
 
 /**
  * Populates a table with attendee information for every 30-minute interval for a specific event in 24-hour mode
- * @param {number} n - Row number of the event in the database 
+ * @param {number} n - Row number of the event in the database
  */
 function makeTable24(n)
 {
@@ -828,7 +1067,7 @@ function makeTable24(n)
 
 /**
  * Locates the given event and organizer combo in the database (displays appropriate error message in case of failure), and displays event information
- * @param {object} form - HTML form element that takes input for Organizer Name, and Event Name 
+ * @param {object} form - HTML form element that takes input for Organizer Name, and Event Name
  */
 function getData(form)
 {
